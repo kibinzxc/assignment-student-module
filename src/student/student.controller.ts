d616eea589685f551767799ae2b0cb0ace749497
Kevin <mamadaddyalexa@gmail.com>
2025-01-11 21:15:19 +0800

    implement CREATE student data functionality

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { StudentService } from './student.service';
+import { CreateStudentDto } from './dto/create-student.dto';
 
-@Controller('student')
-export class StudentController {}
+@Controller('students')
+export class StudentController {
+    constructor(private readonly studentService: StudentService) { }
+
+    @Post()
+    async create(@Body() createStudentDto: CreateStudentDto) {
+        return this.studentService.create(createStudentDto);
+    }
+}
